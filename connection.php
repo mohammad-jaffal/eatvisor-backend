823d45307e1c8c9283f21bb38e56e53c5c71126b
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-27 18:19:40 +0300

    Creating connection file

--- connection.php
+++ connection.php
@@ -1,4 +1,10 @@
 <?php
-echo "hello there"
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "eatvisor_db";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
 ?>
